000b1e3b3dc74ed9c9054ac30e636fd289251b1c
Nick Soto <nvsoto4@gmail.com>
2024-02-24 23:05:27 -0600

    main page

--- mainPage.js
+++ mainPage.js
@@ -1,1 +1,8 @@
-// Write your JavaScript code here
+function initMap() {
+    new google.maps.Map(document.getElementById("map"), {
+      mapId: "844b353f55abd55d",
+      center: { lat: 48.85, lng: 2.35 },
+      zoom: 12,
+    });
+  }
+  window.initMap = initMap;
